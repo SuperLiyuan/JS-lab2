72133048b47c96a43822f8f8e94209cb61c706ec
Liyuan <siiilence@126.com>
2024-02-20 10:02:53 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// Using CommonJS syntax for demonstration. You can switch to ES modules if required by your project setup.
+
+// Importing the utilities file. Replace 'utilities' with the actual name of your utilities file.
+const utils = require('./util');
+
+utils.welcome('Liyuan', 'Luo');
+console.log('Converting Celsius to Fahrenheit:', utils.celsiusToFahrenheit(10));
+console.log('Calculating factorial:', utils.factorial(10));
+console.log('Generating Fibonacci sequence:', utils.fibSequence(10));
+
+
---
+++ util.js
@@ -0,0 +1,43 @@
+
+// First Function: Welcome Message
+function welcome(firstName, lastName) {
+    console.log(`Welcome ${firstName} ${lastName} to G@I College`);
+  }
+  
+  // Second Function: Convert Celsius to Fahrenheit
+  function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+  }
+  
+  // Third Function: Calculate Factorial
+  function factorial(n) {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
+  
+  // Fourth Function: Generate Fibonacci Sequence
+  function fibSequence(n) {
+    // The first two numbers in the Fibonacci sequence
+    const sequence = [0, 1];
+  
+    // Generate the sequence up to n numbers
+    for (let i = 2; i < n; i++) {
+      // Add the sum of the last two numbers to the sequence
+      sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+
+    // Return the first n numbers of the Fibonacci sequence
+    return sequence.slice(0, n);
+  }
+  
+  //commonJS 
+  module.exports = {
+    welcome,
+    celsiusToFahrenheit,
+    factorial,
+    fibSequence
+  };
+  
